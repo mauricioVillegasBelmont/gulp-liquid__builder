1bb5f07dbf7db7504f161867045a044244195624
miau <mauricio@pymeweb.mx>
2025-08-19 12:38:08 -0600

    updated

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ const dartSass = require('sass');
 const gulp = require('gulp');
 const gulpSass = require('gulp-sass');
 const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const typescript = require('gulp-typescript'); // Add this!
-const chokidar = require('chokidar'); // Replace gulp-watch
 const rimraf = require('rimraf');
 const { sync: glob } = require('glob');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano') ;
+
 
 // Sass
 const sass = gulpSass(dartSass);
@@ -31,21 +31,21 @@ const protectedPaths = [
 // Paths
 const paths = {
   styles: {
-    src: 'src/styles/**/*.scss',
+    src: 'src/styles/**/*.{css,scss}',
     dest: 'theme/assets'
   },
   scripts: {
     src: 'src/scripts/**/*.ts', // Now .ts!
     dest: 'theme/assets'
   },
-  images: {
-    src: 'src/**/*.{gif,jpg,png,webp,svg}',
+  assets: {
+    src: 'src/**/*.{gif,jpg,png,webp,svg,woff,woff2}',
     dest: 'theme/assets'
   },
   liquid: {
     src: [
       'src/**/*.{liquid,json,html}', 
-      '!src/**/*.{gif,jpg,png,webp,svg}', 
+      '!src/**/*.{gif,jpg,png,webp,svg,woff,woff2}', 
       '!src/styles/**/*.scss', 
       '!src/scripts/**/*.ts'
     ],
@@ -54,7 +54,7 @@ const paths = {
 };
 
 // Clean task: remove all in theme/ except protected
-function clean(done) {
+function clearTheme() {
   const baseDir = 'theme/';
   
   // Build glob pattern: match everything except protected
@@ -75,22 +75,33 @@ function clean(done) {
   } else {
     console.log('No files to clean.');
   }
-
-  done();
 }
 
 function assets() {
-  return gulp.src(paths.images.src)
-    .pipe(gulp.dest(paths.images.dest));
+  return gulp.src(paths.assets.src)
+    .pipe(rename(path => {
+      path.dirname = '';
+    }))
+    .pipe(gulp.dest(paths.assets.dest));
 }
 
 
 // CSS: SCSS → Tailwind + Autoprefixer + Minify
-function css() {
+function styles() {
+  const plugins = [
+    require('@tailwindcss/postcss')('tailwind.config.js'),
+    autoprefixer(),
+  ];
+  if(process.env.NODE_ENV === 'production') plugins.push(cssnano())
+
   return gulp.src(paths.styles.src)
     .pipe(sass().on('error', sass.logError))
-    .pipe(postcss())
+    .pipe(postcss(plugins))
+    
     // .pipe(rename('theme.css'))
+    .pipe(rename(path => {
+      path.dirname = '';
+    }))
     .pipe(gulp.dest(paths.styles.dest));
 }
 
@@ -103,6 +114,9 @@ function scripts() {
     .js // Get the compiled JS stream
     .pipe(uglify())
     // .pipe(rename('theme.js'))
+    .pipe(rename(path => {
+      path.dirname = '';
+    }))
     .pipe(gulp.dest(paths.scripts.dest));
 }
 
@@ -112,25 +126,31 @@ function liquid() {
     .pipe(gulp.dest(paths.liquid.dest));
 }
 
-// Watch using chokidar (modern, safe)
-function watchFiles() {
-  chokidar.watch(paths.styles.src).on('all', () => gulp.series(css) );
-  chokidar.watch(paths.scripts.src).on('all', () => gulp.series(scripts) );
-  chokidar.watch(paths.liquid.src).on('all', () => gulp.series(liquid) );
-}
 
-//  Clean 
-const cleanFiles = gulp.series(clean);
-// Build task
-const build = gulp.series(clean, gulp.parallel(css, scripts, assets,liquid));
-// Default task
-const dev = gulp.series(build, watchFiles);
 
-// Export tasks
-exports.css = css;
-exports.scripts = scripts;
-exports.liquid = liquid;
-exports.build = build;
-exports.watch = watchFiles;
-exports.clear = cleanFiles;
-exports.default = dev;
+
+
+// Tarea gulp para eliminar directorio de producción
+gulp.task('clear', async () => {
+  clearTheme();
+});
+
+
+gulp.task('assets', async () => {
+    assets();
+});
+gulp.task('css', styles);
+gulp.task('js', scripts);
+gulp.task('liquid', liquid);
+
+// Tarea gulp para producción
+gulp.task('build', gulp.series('clear', 'assets', 'liquid','css', 'js'));
+
+// Tarea gulp para desarrollo
+gulp.task('watch', gulp.series('clear', 'assets', 'liquid','css', 'js', async () => {
+  // watchFiles()
+  gulp.watch(paths.assets.src,  gulp.series('assets'));
+  gulp.watch(paths.liquid.src,  gulp.series('liquid'));
+  gulp.watch(paths.styles.src,  gulp.series('css'));
+  gulp.watch(paths.scripts.src, gulp.series('js'));
+}));
--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-    cssnano: process.env.NODE_ENV === 'production' ? {} : false, // minify in prod
-  },
-}
